d3b6346093b25a39bc453481ad2d855df09dba8f
cr-tungnt <tung.tien@cybereason.com>
2024-01-24 12:25:54 +0700

    fix i18n

--- src/i18n/i18n.module.ts
+++ src/i18n/i18n.module.ts
@@ -12,7 +12,8 @@ import { DEFAULT_LANGUAGE } from '../common/constants';
         NestI18nModule.forRoot({
             fallbackLanguage: DEFAULT_LANGUAGE,
             loaderOptions: {
-                path: path.join('src/i18n'),
+                path: path.join('dist/i18n'),
+                // path: path.join('src/i18n'),
                 watch: true,
             },
             resolvers: [AcceptLanguageResolver],
