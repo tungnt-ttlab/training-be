25d5e0a6fcaf22cfdfec4f8e968e8d6cd55ec983
cr-tungnt <tung.tien@cybereason.com>
2024-01-31 20:00:45 +0700

    fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,6 +12,15 @@ export class AppController {
 
     @Options('*')
     handleOptions() {
-        // Xử lý Preflight request
+        return {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': 'https://training-fe.vercel.app', // Thay đổi thành origin của bạn
+                'Access-Control-Allow-Methods':
+                    'GET,HEAD,PUT,PATCH,POST,DELETE',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Credentials': 'true',
+            },
+        };
     }
 }
