c049c47e67d4caa6430f245071101a3ab953f795
cr-tungnt <tung.tien@cybereason.com>
2024-01-31 20:04:35 +0700

    fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Options } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Ping API')
@@ -8,19 +8,5 @@ export class AppController {
     @Get('ping')
     pingAlive() {
         return 'pong';
-    }
-
-    @Options('*')
-    handleOptions() {
-        return {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': 'https://training-fe.vercel.app', // Thay đổi thành origin của bạn
-                'Access-Control-Allow-Methods':
-                    'GET,HEAD,PUT,PATCH,POST,DELETE',
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Credentials': 'true',
-            },
-        };
     }
 }
