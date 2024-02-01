ae00b38edd418dd6cb7660f13a063c0c9d9fc1e9
cr-tungnt <tung.tien@cybereason.com>
2024-02-01 14:58:16 +0700

    fix-ahihi

--- src/modules/common/transform.interceptor.ts
+++ src/modules/common/transform.interceptor.ts
@@ -25,9 +25,6 @@ export class TransformInterceptor implements NestInterceptor {
                 }).includes(data?.code)
                     ? data?.code
                     : null;
-                console.log(
-                    JSON.stringify(context.switchToHttp().getResponse()),
-                );
 
                 if (httpCode) {
                     context.switchToHttp().getResponse().status(httpCode);
